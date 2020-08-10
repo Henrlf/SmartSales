428c04a1d38678c16929ad308d7ac9557638f0b4
Keda237 <henriquefraneschini@hotmail.com>
2020-08-10 15:50:33 -0300

    ManyToMany funcional(eu acho). Id da tabela produtos_pedido removido, utilizar o id do pedido para a localização. "funcao" da tabela funcionario, foi alterada para "cargo"

--- SmartSales/src/apoio/Criptografia.java
+++ SmartSales/src/apoio/Criptografia.java
@@ -1,7 +1,21 @@
 package apoio;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
 public class Criptografia {
     
-    
+        public static String encriptografar(String senha) {
+        String senhacrip = "";
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+            BigInteger hash = new BigInteger(1, md.digest(senha.getBytes()));
+            senhacrip = hash.toString(16);
+        } catch (Exception e) {
+            
+        }
+        return senhacrip;
+    }
     
 }
