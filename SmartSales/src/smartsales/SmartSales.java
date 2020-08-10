1639f1ef7fd939c1c9a3e3ce07fb910e049b695b
yNot <yNot@DESKTOP-G9F95O4>
2020-08-10 20:30:07 -0300

    Commit 2 teste do teste

--- SmartSales/src/smartsales/SmartSales.java
+++ SmartSales/src/smartsales/SmartSales.java
@@ -8,21 +8,10 @@ public class SmartSales {
 
     public static void main(String[] args) {
 
-        
-        try {
-            //here you can put the selected theme class name in JTattoo
+        try{
             UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
-
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }catch (Exception e){ 
         }
-
             Tela_Login log = new Tela_Login();
             log.setVisible(true);
             log.setLocationRelativeTo(null);
