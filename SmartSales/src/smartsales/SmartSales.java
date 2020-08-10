2f8a9ca8b139e7f4b7d9ce26a733d109fa408bd8
yNot <yNot@DESKTOP-G9F95O4>
2020-08-10 20:21:48 -0300

    Commit 2 teste do teste

--- SmartSales/src/smartsales/SmartSales.java
+++ SmartSales/src/smartsales/SmartSales.java
@@ -1,15 +1,31 @@
 package smartsales;
 
-import telas.Tela_CadastroFuncionario;
+import javax.swing.UIManager;
+import telas.Tela_Login;
+
 
 public class SmartSales {
 
     public static void main(String[] args) {
 
-        Tela_CadastroFuncionario cd = new Tela_CadastroFuncionario();
-        cd.setVisible(true);
-        cd.setLocationRelativeTo(null);
+        
+        try {
+            //here you can put the selected theme class name in JTattoo
+            UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
 
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Tela_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+            Tela_Login log = new Tela_Login();
+            log.setVisible(true);
+            log.setLocationRelativeTo(null);
     }
 
 }
