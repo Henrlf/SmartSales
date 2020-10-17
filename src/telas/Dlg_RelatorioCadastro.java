9bf5e07c105aaa13e74b34b531f6edbe0fe5411a
yNot <yNot@DESKTOP-G9F95O4>
2020-10-17 09:01:55 -0300

    Nada Funciona ainda só criei umas telas pra passar parametros

--- src/telas/Dlg_RelatorioCadastro.java
+++ src/telas/Dlg_RelatorioCadastro.java
@@ -41,8 +41,8 @@ public class Dlg_RelatorioCadastro extends javax.swing.JDialog {
 
         jButton4 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        comboCadastro = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
+        comboCadastro = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Relatório de Cadastros");
@@ -63,41 +63,41 @@ public class Dlg_RelatorioCadastro extends javax.swing.JDialog {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel1.setText("Selecione o tipo de Cadastro:");
+
         comboCadastro.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         comboCadastro.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", "Cliente", "Funcionário", "Cidade", "Produto" }));
-
-        jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
-        jLabel1.setText("Seleciona o tipo de Cadastro:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(47, 47, 47)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1)
-                    .addComponent(comboCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(46, Short.MAX_VALUE))
+                .addGap(54, 54, 54)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(comboCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addContainerGap(51, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
-                .addGap(17, 17, 17)
+                .addGap(21, 21, 21)
                 .addComponent(jButton4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(24, 24, 24))
+                .addGap(25, 25, 25))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(28, 28, 28)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(comboCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 42, Short.MAX_VALUE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         pack();
@@ -107,28 +107,92 @@ public class Dlg_RelatorioCadastro extends javax.swing.JDialog {
 
         if (!comboCadastro.getSelectedItem().toString().equals("Selecione")) {
 
-            try {
-                // Compila o relatorio
-                JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/SmartSales/src/Relatorios/Cadasros"));
+            if (comboCadastro.getSelectedItem().toString().equals("Cliente")) {
+                try {
+                    // Compila o relatorio
+                    JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/SmartSales/src/Relatorios/Cliente"));
 
-                // Mapeia campos de parametros para o relatorio, mesmo que nao existam
-                Map parametros = new HashMap();
-                parametros.put("cadastro", comboCadastro.getSelectedItem().toString());
+                    // Mapeia campos de parametros para o relatorio, mesmo que nao existam
+                    Map parametros = new HashMap();
+                    parametros.put("cadastro", comboCadastro.getSelectedItem().toString());
 
-                // Executa relatoio
-                JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, ConexaoBD.getInstance().getConnection());
+                    // Executa relatoio
+                    JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, ConexaoBD.getInstance().getConnection());
 
-                // Exibe resultado em video
-                JasperViewer.viewReport(impressao, false);
+                    // Exibe resultado em video
+                    JasperViewer.viewReport(impressao, false);
 
-                //dispose();
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+                    //dispose();
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+                }
+
+            }
+            if (comboCadastro.getSelectedItem().toString().equals("Funcionário")) {
+                try {
+                    // Compila o relatorio
+                    JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/SmartSales/src/Relatorios/Funcionario"));
+
+                    // Mapeia campos de parametros para o relatorio, mesmo que nao existam
+                    Map parametros = new HashMap();
+                    parametros.put("cadastro", comboCadastro.getSelectedItem().toString());
+
+                    // Executa relatoio
+                    JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, ConexaoBD.getInstance().getConnection());
+
+                    // Exibe resultado em video
+                    JasperViewer.viewReport(impressao, false);
+
+                    //dispose();
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+                }
+            }
+            if (comboCadastro.getSelectedItem().toString().equals("Cidade")) {
+                try {
+                    // Compila o relatorio
+                    JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/SmartSales/src/Relatorios/Cidade"));
+
+                    // Mapeia campos de parametros para o relatorio, mesmo que nao existam
+                    Map parametros = new HashMap();
+                    parametros.put("cadastro", comboCadastro.getSelectedItem().toString());
+
+                    // Executa relatoio
+                    JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, ConexaoBD.getInstance().getConnection());
+
+                    // Exibe resultado em video
+                    JasperViewer.viewReport(impressao, false);
+
+                    //dispose();
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+                }
+            }
+            if (comboCadastro.getSelectedItem().toString().equals("Produto")) {
+                try {
+                    // Compila o relatorio
+                    JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream("/SmartSales/src/Relatorios/Produto"));
+
+                    // Mapeia campos de parametros para o relatorio, mesmo que nao existam
+                    Map parametros = new HashMap();
+                    parametros.put("cadastro", comboCadastro.getSelectedItem().toString());
+
+                    // Executa relatoio
+                    JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, ConexaoBD.getInstance().getConnection());
+
+                    // Exibe resultado em video
+                    JasperViewer.viewReport(impressao, false);
+
+                    //dispose();
+                } catch (Exception e) {
+                    JOptionPane.showMessageDialog(null, "Erro ao gerar relatório: " + e);
+                }
             }
 
         } else {
             JOptionPane.showMessageDialog(null, "Selecione um Cadastro Valido");
         }
+
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
